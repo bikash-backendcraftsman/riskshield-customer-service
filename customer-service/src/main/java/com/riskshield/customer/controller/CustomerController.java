e7d8b76bde1cdb73451b6bd80e37659ac5b99731
bikash-backendcraftsman <bikash.backendcraftsman@gmail.com>
2026-01-18 00:05:12 +0530

    feat(customer-service): introduce CustomerController for customer management APIs

---
+++ customer-service/src/main/java/com/riskshield/customer/controller/CustomerController.java
@@ -0,0 +1,10 @@
+package com.riskshield.customer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/customers/")
+public class CustomerController {
+
+}
